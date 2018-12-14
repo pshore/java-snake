0dcc80e2a306144008934d3beba65e312d4eb857
pshore <pshore2@gmail.com>
2018-12-14 11:04:55 +0000

    Refactor VtAnsiScreenOutputter to be cleaner code and easier to follow.

--- src/main/java/com/gmail/pshore/snake/clientio/VtAnsiScreenOutputter.java
+++ src/main/java/com/gmail/pshore/snake/clientio/VtAnsiScreenOutputter.java
@@ -17,36 +17,56 @@ public class VtAnsiScreenOutputter implements ScreenOutputter {
 		this.out = out;
 	}
 	
+	
 	/** Outputs one frame of the game action. */
 	@Override
 	public void outputScreen(GameScreenGrid screenGrid) {
 				
+		startScreenOutput();
+		
+		for(ScreenObject screenObject : screenGrid.getLayeredScreenObjects() )
+			outputScreenObject(screenObject);
+		
+		endScreenOutput();
+		
+	}
+
+	/** Clears the screen and prepares the terminal for outputting each ScreenObject. */
+	private void startScreenOutput() {
 		out.print( VtAnsi.attribute(VtAnsi.ATTR_HIDDEN, VtAnsi.ATTR_FG_RED) );	// hide the cursor
 		out.print( VtAnsi.eraseAndHome() ); 				// clear the entire screen
-		
-		for(ScreenObject screenObject : screenGrid.getLayeredScreenObjects() ) {
+	}
 
-			List<Gridref> allGridrefs = screenObject.getPositions();
-			for( int i=0 ; i< allGridrefs.size() ; i++ ) {
-				
-				Gridref gridref = allGridrefs.get(i);
-				int column = gridref.getX();
-				int line   = gridref.getY();
-
-				out.print( VtAnsi.cursorTo(line, column) );
-				
-				// later we'll push this into separate classes
-				if(screenObject instanceof SnakeCharacter) {
-					if( i==0 )
-						out.print('%'); // the head char
-					else
-						out.print('O'); // the body char
-				}
-			}
-		}
+	/** Do any final tasks to the terminal to leave a good looking screen. */
+	private void endScreenOutput() {
 		out.print( VtAnsi.cursorTo(0, 0) );
 		out.print( VtAnsi.attribute(VtAnsi.ATTR_HIDDEN) );	// hide the cursor
-		
+	}
+	
+	/** 
+	 * Write a single ScreenObject (eg a Snake) to the terminal.
+	 * 
+	 * @param screenObject
+	 */
+	private void outputScreenObject(ScreenObject screenObject) 
+	{
+		List<Gridref> allGridrefs = screenObject.getPositions();
+		for( int i=0 ; i< allGridrefs.size() ; i++ ) {
+			
+			Gridref gridref = allGridrefs.get(i);
+			int column = gridref.getX();
+			int line   = gridref.getY();
+
+			out.print( VtAnsi.cursorTo(line, column) );
+			
+			// later we'll push this into separate classes
+			if(screenObject instanceof SnakeCharacter) {
+				if( i==0 )
+					out.print('%'); // the head char
+				else
+					out.print('O'); // the body char
+			}
+		}
 	}
 
 
