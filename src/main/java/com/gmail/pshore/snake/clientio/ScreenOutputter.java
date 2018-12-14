35a3d3699c14b26a620cc38dc595449b09c1dca6
pshore <pshore2@gmail.com>
2018-12-14 11:11:03 +0000

    Added javadocs

--- src/main/java/com/gmail/pshore/snake/clientio/ScreenOutputter.java
+++ src/main/java/com/gmail/pshore/snake/clientio/ScreenOutputter.java
@@ -2,8 +2,18 @@ package com.gmail.pshore.snake.clientio;
 
 import com.gmail.pshore.snake.game.GameScreenGrid;
 
+/**
+ * For classes that show a textual view of the Game.
+ * 
+ * @author Phil Shore pshore2@gmail.com
+ */
 public interface ScreenOutputter {
 
+	/**
+	 * Trigger an output of one frame of the game.
+	 * 
+	 * @param screenGrid The current state of the characters in the game.
+	 */
 	void outputScreen(GameScreenGrid screenGrid);
 	
 }
